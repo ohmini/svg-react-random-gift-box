fb084d5e4d63017e8e2ce76ade1bfb1362b5ca81
Chonticha Chang <ruttikar_maya@hotmail.com>
2018-05-03 15:38:45 +0700

    Update ItemContainer, use position instead of grid, using weight and height

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -8,15 +8,15 @@ const Container = styled.g`
 
 class ItemContainer extends Component {
   render() {
-    const {ItemComponent, grid, handleClick, isMoving} = this.props
+    const {ItemComponent, position, width, height, handleClick, isMoving} = this.props
     return (
       <Container>
         <ItemComponent
           handleClick={handleClick}
-          x={grid.x}
-          y={grid.y}
-          width={grid.width}
-          height={grid.height}
+          x={position.x}
+          y={position.y}
+          width={width}
+          height={height}
           isMoving={isMoving}
         />
       </Container>
